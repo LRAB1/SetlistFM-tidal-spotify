db8adbbbca8879003b4f32ac4d3b3863814bf9fe
LRAB1 <110101676+LRAB1@users.noreply.github.com>
2022-09-06 14:52:53 +0200

    first functionality

--- request.js
+++ request.js
@@ -1,17 +1,16 @@
-const https = require('https')
+var unirest = require('unirest');
 
-//With headers this request gets refused in an ECONNREFUSED error
-const url = "https://api.setlist.fm/rest/1.0/setlist/4bb7d3c2";
+const urlWithId = "https://api.setlist.fm/rest/1.0/setlist/4bb7d3c2";
+const urlWithoutId = "https://api.setlist.fm/rest/1.0/setlist/";
+const iD = '4bb7d3c2'
 
-https.get(url, res => {
-  let data = '';
-  res.on('data', chunk => {
-    data += chunk;
-  });
-  res.on('end', () => {
-    data = JSON.parse(data);
-    console.log(data);
+var req = unirest('GET', urlWithoutId + iD)
+  .headers({
+    'accept': 'application/json',
+    'x-api-key': 'NAfOueu-etj29FHm3uehzhq5_Ipr3pD68Ii5'
   })
-}).on('error', err => {
-  console.log(err.message);
-});
+  .send("")
+  .end(function (res) { 
+    if (res.error) throw new Error(res.error); 
+    console.log(res.raw_body);
+  });
