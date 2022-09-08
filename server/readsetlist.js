e4a054023c1e5aa728f201eaa4a9d4e58fa41ded
LRAB1 <110101676+LRAB1@users.noreply.github.com>
2022-09-08 18:21:40 +0200

    WORKY better

--- server/readsetlist.js
+++ server/readsetlist.js
@@ -2,26 +2,33 @@
 const fs = require('fs');
 const { stringify } = require('querystring');
 
-const songs = [];
-
 fs.readFile('./data/raw-setlist.json', (err, setlist) => {
     if (err) throw err;
-    let localsetlist = JSON.parse(setlist);
+    const localsetlist = JSON.parse(setlist);
     
     console.log('Finished reading file.');
 
     //Artist name gets pulled in json format.
-    let artist = localsetlist.artist.name
+    const artist = localsetlist.artist.name
     console.log(artist);
 
     //Grabbing tour name for playlist naming
-    let tour = localsetlist.tour.name
+    const tour = localsetlist.tour.name
     console.log(tour);
 
-    //Trying to extract song names. Runs into name being undefined error.
-    let songs = JSON.stringify(localsetlist.sets.set/*.song.name*/);
-    console.log(songs);
+    //Check how many set arrays exist.
+    const amountSets = (localsetlist.sets.set.length);
+    console.log('Amount of sets played: ' + amountSets);
+
+
+    //Trying to extract song amount and names
+    const songAmount = localsetlist.sets.set[0].song;
+    const encores = localsetlist.sets.set[1].song;
+    console.log(`Amount of songs in setlist: `+ songAmount.length);
+    console.log('Amount of songs in encore: ' + encores.length);
+    console.log(songAmount);
+    console.log(encores);
 
     //Create setlist.txt, containing artis, tourname and songlist(json unfortunately).
-    fs.writeFileSync('./data/setlist.txt', artist + "\r\n" +  tour + "\r\n" + songs);
+    fs.writeFileSync('./data/setlist.txt', artist + "\r\n" +  tour + "\r\n" + JSON.stringify(songAmount) + "\r\n" + JSON.stringify(encores));
 });
