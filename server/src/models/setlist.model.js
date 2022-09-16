ebb9a401bd1cb8966f37dc32d5121184f24d994e
LRAB1 <110101676+LRAB1@users.noreply.github.com>
2022-09-16 13:01:26 +0200

    server prep for spotify intregration

---
+++ server/src/models/setlist.model.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const { stringify } = require('querystring');
+
+fs.readFile('././data/raw-setlist.json', (err, setlist) => {
+    if (err) throw err;
+    const localsetlist = JSON.parse(setlist);
+    console.log('Finished reading file.');
+
+    //Artist name gets pulled in json format.
+    const artist = localsetlist.artist.name
+    //console.log(artist);
+
+    //Grabbing tour name for playlist naming.
+    const tour = [];
+    if (!localsetlist.tour) {
+        console.log('No tour found.')
+    } else tour.push(localsetlist.tour.name)
+    //console.log(tour);
+
+    //Check how many set arrays exist and therefore if an encore was performed.
+    const amountSets = [];
+    amountSets.push(localsetlist.sets.set.length);
+    console.log('amount of sets', amountSets);
+
+    //Grabbing songs and names.
+    const songs = [];
+    if ([amountSets] == 0 ) {
+        console.log('Information not known to SetlistFM.');
+    } else if  ([amountSets] == 1 ) {
+        console.log('No encore was performed');
+        songs.push(localsetlist.sets.set[0].song);
+    } else if ([amountSets] == 2 ) {
+        console.log('An encore was performed');
+        songs.push(localsetlist.sets.set[0].song);
+        songs.push(localsetlist.sets.set[1].song);
+        } else if ([amountSets] == 3) {
+            console.log('More than 2 sets?! SHEESH')
+            songs.push(localsetlist.sets.set[0].song);
+            songs.push(localsetlist.sets.set[1].song);
+            songs.push(localsetlist.sets.set[2].song);
+                } else if ([amountSets] == 4 ) {
+                    console.log('More than 3 sets?! SHEESH')
+                    songs.push(localsetlist.sets.set[0].song);
+                    songs.push(localsetlist.sets.set[1].song);
+                    songs.push(localsetlist.sets.set[2].song);
+                    songs.push(localsetlist.sets.set[3].song);
+    }
+    //Create setlist.txt, containing artist, tourname and songlist(json unfortunately).
+    fs.writeFileSync('././data/setlist.txt', artist + "\r\n" +  tour + "\r\n" + JSON.stringify(songs,null, 4));    
+});
--- server/src/routes/setlist.controller.js
+++ server/src/routes/setlist.controller.js
@@ -1,25 +1,23 @@
 const fs = require('fs');
 
 var unirest = require('unirest');
-const SetlistFmKey = require('../keys');
+const setlistFmKey = require('../../../keys');
 
-//const urlWithId = "https://api.setlist.fm/rest/1.0/setlist/4bb7d3c2"; //Aurora set 3bb0fc9c
+//const urlWithId = "https://api.setlist.fm/rest/1.0/setlist/4bb7d3c2"; //Aurora set 3bb0fc9c //Stones set met 4 entries 3d69d57
 const urlWithoutId = "https://api.setlist.fm/rest/1.0/setlist/";
-const iD = '3bb0fc9c';
-
-const API_KEY = SetlistFmKey
+const iD = '63b2aa3f';
 
 var req = unirest('GET', urlWithoutId + iD)
   .headers({
     'accept': 'application/json',
-    'x-api-key': SetlistFmKey
+    'x-api-key': setlistFmKey
   })
   .send("")
   .end(function (res) { 
     if (res.error) throw new Error(res.error); 
     console.log(res.raw_body);
     let data = res.raw_body;
-    fs.writeFileSync('./data/raw-setlist.json', data);
+    fs.writeFileSync('././data/raw-setlist.json', data);
     console.log(`Data written to file!`)
   });
 
