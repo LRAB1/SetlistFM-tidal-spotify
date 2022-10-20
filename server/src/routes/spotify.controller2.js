ecb9ba26b5d2e9277379093749d5d6c7d464f7b6
LRAB1 <110101676+LRAB1@users.noreply.github.com>
2022-10-20 16:35:57 +0200

    updoot

--- server/src/routes/spotify.controller2.js
+++
@@ -1,8 +0,0 @@
-//import not ready yet, returns undefined.
-
-const { artist,
-        tour,
-        songs,
-} = require('../models/setlist.model');
-
-console.log(`Artist: ${artist} Tour: ${tour} Songs: ${songs}`);
